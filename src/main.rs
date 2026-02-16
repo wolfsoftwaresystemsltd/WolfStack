235343beb364cefda439d6a4060b171386bfc3bf
Paul C <paulc@wolf.uk.com>
2026-02-16 11:35:05 +0000

    chore: bump version to v9.4.2

--- src/main.rs
+++ src/main.rs
@@ -268,6 +268,8 @@ async fn main() -> std::io::Result<()> {
             loop {
                 tokio::time::sleep(Duration::from_secs(10)).await;
                 agent::poll_remote_nodes(cluster_poll.clone(), secret_poll.clone(), Some(ai_agent_poll.clone())).await;
+                // Sync container routes from WolfNet peers (works without cluster membership)
+                containers::sync_wolfnet_peer_routes().await;
             }
         });
 
